2a9a27755ee828474c72c33f33b6d0d59934bc55
Joyce <racechao@gmail.com>
2015-11-08 23:56:53 +0800

    create a tableViewController to list animations

---
+++ iOS-Animations/BaseViewAnimationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BaseViewAnimationViewController.swift
+//  iOS-Animations
+//
+//  Created by race on 15/11/8.
+//  Copyright © 2015年 alfredcc. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewAnimationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ iOS-Animations/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Animations
+//
+//  Created by race on 15/11/8.
+//  Copyright © 2015年 alfredcc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
